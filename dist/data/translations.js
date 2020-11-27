d616a35797a631ad5784f0421022ecdcf5d69939
Miguel Rivero <MiguelArturo.Rivero@nestle.com>
2020-11-27 15:45:33 +0100

    script -> data folder renamed

---
+++ dist/data/translations.js
@@ -0,0 +1,11 @@
+window.CoffeePLPBannerCopy = {
+  au: {
+    heading: {
+      en: `Not a <strong>Nespresso</strong> Professional Member?`,
+    },
+    subtext: {
+      en: `Before placing a coffee order, you should become an owner of a <strong>Nespresso</strong> Professional coffee machine.`,
+    },
+    cta: { en: `SEE ALL MACHINES` },
+  },
+};
